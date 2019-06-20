0ffad7e11174e74ef60fc0c9ba8de1bb3465cba4
bhargav-s-271100 <51982154+bhargav-s-271100@users.noreply.github.com>
2019-06-20 14:16:49 +0530

    Add files via upload

--- pyton.py
+++ pyton.py
@@ -1,1 +1,12 @@
-
+def mine():
+   sname=input('enter the servername')
+   dbname=input('enter the dbname')
+   uname=input('enter the username')
+   passw=input('enetr the password')
+   s=sname+dbname+uname+passw
+   return s
+def main():
+    p=mine()
+main()    
+for i in p:
+      print('%s'.format(i))
