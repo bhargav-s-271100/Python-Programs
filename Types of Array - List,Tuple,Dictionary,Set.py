b4c11c563894264925ac229d6dd8b535623b53c3
Bhargav S <51982154+bhargav-s-271100@users.noreply.github.com>
2019-08-11 20:52:38 +0530

    Add files via upload

---
+++ Types of Array - List,Tuple,Dictionary,Set.py
@@ -0,0 +1,17 @@
+print("Types Of Array : LIST")
+a=[1,2,3,4]
+a[2]=5
+print(a)
+print("Types Of Array : TUPLE")
+a=(1,2,3,4,2)
+print(a)
+print("Types Of Array : DICTIONARY")
+a={1:'bat',2:'ball'}
+print(a)
+print(a[1])
+print(a[2])
+print("Types Of Array : SET")
+a={1,'bat','ball',53}
+print(a)
+
+
