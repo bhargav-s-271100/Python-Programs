94cb112377427aed864ce74f4f34855de2023c5f
Bhargav S <51982154+bhargav-s-271100@users.noreply.github.com>
2019-08-16 07:14:04 +0530

    Add files via upload

---
+++ Patterns using for loop.py
@@ -0,0 +1,14 @@
+for i in range(6):
+    print("\n")
+    for j in range(6):
+        print("*",end="")
+for i in range(6):
+    print("\n")
+    for j in range(i):
+        print("*",end="")
+for i in range(6):
+    print("\n")
+    j=6
+    while(j>i):
+        print("*",end="")
+        j-=1
