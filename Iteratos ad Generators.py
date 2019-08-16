eb2b1807b9047e163b0b4f8cf8df8b03bb1d5012
Bhargav S <51982154+bhargav-s-271100@users.noreply.github.com>
2019-08-16 07:12:57 +0530

    Add files via upload

---
+++ Iteratos ad Generators.py
@@ -0,0 +1,14 @@
+print("WHILE LOOP")
+i=0
+while i<10:
+    print("*\n")
+    i=i+1
+print("FOR LOOP")
+a=[1,2,3,4,5]
+for i in a:
+    print("*\n")
+print("GENERATORS in FOR loop")
+for i in range(0,10,2):
+    print("*\n")
+for i in range(-1,-6,-1):
+    print("*\n")
