1139905d0cba6fdb428a6bcd75badad1fb9ebb21
Bhargav S <bhargavs271100@gmail.com>
2019-10-15 07:45:00 +0530

    Add files via upload

---
+++ FILE HANDLING.py
@@ -0,0 +1,19 @@
+# To create and write in a text
+file=open("new.txt","w")
+file.write("My First Text \n")
+file.write("My Second Text ")
+file.close()
+# here file is owerwritten again and again
+# so we append to add the code
+file=open("appended.txt","a")
+file.write("My First Text \n")
+file.write("My Second Text ")
+file.close()
+# to read a file
+file=open("new.txt","r")
+print(file.read())
+# to write user input data to a file
+file=open("appended.txt","a")
+a=input("Enter the data ")
+file.write("Entered data = %s" %a)
+file.close()
