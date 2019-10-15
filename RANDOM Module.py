92ef28ece3e0aeb812ed7f55e91712fe66d89208
Bhargav S <bhargavs271100@gmail.com>
2019-10-15 07:45:46 +0530

    Add files via upload

---
+++ RANDOM Module.py
@@ -0,0 +1,11 @@
+import random
+a=random.randint(0,100)
+print(a)
+a=random.random()
+print(a)
+a=random.randrange(1,50)
+print(a)
+a=[1,3,7,13,21,31]
+random.shuffle(a)
+print(a)
+print(random.choice(a))
