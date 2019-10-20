e1b097cc13bf6f7c1631200e0be03ce1e6b91b7c
Bhargav S <bhargavs271100@gmail.com>
2019-10-20 09:47:42 +0530

    Add files via upload

---
+++ SYS Module.py
@@ -0,0 +1,11 @@
+import sys
+#to get copyright status of python used
+print(sys.copyright)
+#toprint path of python's storage location
+print(sys.path)
+#to exit program from current line
+# sys.exit()
+#to get input from user without using input()
+input=sys.stdin.readline()
+print("Input Given is : ",input)
+#sys.argv is used to give parameters during runtime in cmd interpretor OS like LINUX
